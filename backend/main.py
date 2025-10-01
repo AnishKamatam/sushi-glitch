a68cfc17f3329f1bddad1c5f3708f09603b30e4d
AnishKamatam <anishkamatam22@gmail.com>
2025-10-01 00:40:56 -0700

    deploy using quick tunnels + PWA

--- backend/main.py
+++ backend/main.py
@@ -124,4 +124,4 @@ async def upload_image(image: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
